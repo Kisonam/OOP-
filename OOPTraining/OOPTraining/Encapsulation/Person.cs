b0fdd14e9d67937518d44eb171fc3bc35f6f5ef5
Kisonam <kisonamplay@gmail.com>
2024-02-04 20:35:15 +0100

    Add first simple oop example

---
+++ OOPTraining/OOPTraining/Encapsulation/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace OOPTraining.Encapsulation
+{
+    public class Person
+    {
+        // Приватні поля (дані), які будуть інкапсульовані
+        private string? name;
+        private int age;
+
+        // Властивість для доступу до поля name ззовні класу
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                // Можна встановити додаткові логіки перевірки перед присвоєнням значення
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ім'я не може бути порожнім.");
+                }
+            }
+        }
+
+        // Властивість для доступу до поля age ззовні класу
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                // Можна встановити додаткові логіки перевірки перед присвоєнням значення
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Вік не може бути від'ємним.");
+                }
+            }
+        }
+
+        // Конструктор класу
+        public Person(string name, int age)
+        {
+            // Використання властивостей для інкапсуляції доступу до полів
+            Name = name;
+            Age = age;
+        }
+
+        // Метод, який виводить інформацію про особу
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Ім'я: {Name}, Вік: {Age}");
+        }
+    }
+
+}
+
---
+++ OOPTraining/OOPTraining/Program.cs
@@ -0,0 +1,19 @@
+﻿using OOPTraining.Encapsulation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Створення об'єкта класу Person
+        Person person = new Person("Іван", 25);
+
+        // Звертання до властивостей для отримання та встановлення значень
+        person.Name = "Sofia";
+        person.Age = 30;
+
+        // Виклик методу для виведення інформації
+        person.DisplayInfo();
+
+        Console.ReadKey();
+    }
+}
